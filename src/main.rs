bf8555ba1cb0081e2f930ce1c3b883c516a42867
Max <colten478@gmail.com>
2023-06-04 17:53:09 +0300

    User can choose count of iteration. Unavailable host or ip will be removed after getting an error.

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,7 @@ fn main() {
         option = -1;
         let line = scan_string_with_message(&("Enter an option\n".to_owned()
         + "1) Get info\n2) Connect\n3) Disconnect\n4) Get ping\n5) Check selected hosts ping\n"
-        + "6) Check selected ips\n0) Exit\n: "), false);
+        + "6) Check selected ips\n0) Exit\n"), false);
         match line.parse::<i8>() {
             Ok(num) => {
                 option = num;
@@ -63,8 +63,8 @@ fn main() {
             }
             2 => {
                 let interface = wifi::action::get_interface();
-                let name = scan_string_with_message("Enter the name of your wifi\n: ", false);
-                let pass = scan_string_with_message("Enter your password\n: ", true);
+                let name = scan_string_with_message("Enter the name of your wifi\n", false);
+                let pass = scan_string_with_message("Enter your password\n", true);
                 wifi::action::connect(interface, &name, &pass, true);
             }
             3 => {
@@ -73,68 +73,123 @@ fn main() {
             }
             4 => {
                 let hosts: Vec<&str> = vec!["google.com", "youtube.com", "reddit.com", "github.com"];
-                let mut sum = Duration::ZERO;
-                for host in &hosts {
-                    let dur = ping::get_ping(host, false);
-                    match dur {
-                        Ok(duration) => {
-                            sum += duration;
-                            println!("{} - {:?}", host, duration);
-                        }
-                        Err(err) => {
-                            println!("{}({})", err, host);
+                let count_str = scan_string_with_message("Enter count of iteration\n", false);
+                let mut count = 1;
+                match count_str.parse::<i32>() {
+                    Ok(num) => {
+                        count = num;
+                    },
+                    Err(_) => {}
+                }
+                for i in 0..count {
+                    let mut sum = Duration::ZERO;
+                    for host in &hosts {
+                        let dur = ping::get_ping(host, false);
+                        match dur {
+                            Ok(duration) => {
+                                sum += duration;
+                                // println!("{} - {:?}", host, duration);
+                            }
+                            Err(err) => {
+                                println!("{}({})", err, host);
+                            }
                         }
                     }
-                }
-                if !sum.is_zero() {
-                    println!("Avarage - {:?}", sum/hosts.len().try_into().unwrap());
+                    if !sum.is_zero() {
+                        println!("Ping({}) - {:?}", i, sum/hosts.len().try_into().unwrap());
+                    }
                 }
                 scan_string_with_message("", false);
             }
             5 => {
                 let hosts_str = scan_string_with_message("Enter host\n", false);
-                let hosts = hosts_str.split(' ');
-                let mut sum = Duration::ZERO;
-                let mut count = 0;
-                for host in hosts {
-                    let dur = ping::get_ping(host, false);
-                    match dur {
-                        Ok(duration) => {
-                            sum += duration;
-                            count += 1;
-                            println!("{} - {:?}", host, duration);
-                        }
-                        Err(err) => {
-                            println!("{}({})", err, host);
+                let mut hosts: Vec<String> = hosts_str.split(' ').map(|s| s.to_string()).collect();
+                let count_str = scan_string_with_message("Enter count of iteration\n", false);
+                let mut count = 1;
+                match count_str.parse::<i32>() {
+                    Ok(num) => {
+                        count = num;
+                    },
+                    Err(_) => {}
+                }
+                for i in 0..count {
+                    let mut sum = Duration::ZERO;
+                    let mut count = 0;
+                    let mut remove_list: Vec<usize> = vec![];
+                    for el in 0..hosts.len() {
+                        let dur = ping::get_ping(&hosts[el], false);
+                        match dur {
+                            Ok(duration) => {
+                                sum += duration;
+                                count += 1;
+                            }
+                            Err(err) => {
+                                println!("{}({})", err, hosts[el]);
+                                remove_list.push(el);
+                            }
                         }
                     }
+                    let mut offset = 0;
+                    for el in remove_list {
+                        hosts.remove(el-offset);
+                        offset += 1;
+                    }
+                    if !sum.is_zero() {
+                        println!("Ping({}) - {:?}", i, sum/count);
+                    }
+                    std::thread::sleep(Duration::from_secs(1));
                 }
-                if !sum.is_zero() {
-                    println!("Avarage - {:?}", sum/count);
+                print!("Working hosts:");
+                for host in hosts {
+                    print!(" {}", host);
                 }
+                println!();
                 scan_string_with_message("", false);
             }
             6 => {
                 let ips_str = scan_string_with_message("Enter host\n", false);
-                let ips = ips_str.split(' ');
-                let mut sum = Duration::ZERO;
-                let mut count = 0;
-                for ip in ips {
-                    let dur = ping::get_ping(ip, true);
-                    match dur {
-                        Ok(duration) => {
-                            sum += duration;
-                            count += 1;
-                            println!("{} - {:?}", ip, duration);
-                        }
-                        Err(err) => {
-                            println!("{}({})", err, ip);
+                let mut ips: Vec<String> = ips_str.split(' ').map(|s| s.to_string()).collect();
+                let count_str = scan_string_with_message("Enter count of iteration\n", false);
+                let mut count = 1;
+                match count_str.parse::<i32>() {
+                    Ok(num) => {
+                        count = num;
+                    },
+                    Err(_) => {}
+                }
+                for i in 0..count {
+                    let mut sum = Duration::ZERO;
+                    let mut count = 0;
+                    let mut remove_list: Vec<usize> = vec![];
+                    for el in 0..ips.len() {
+                        let dur = ping::get_ping(&ips[el], true);
+                        match dur {
+                            Ok(duration) => {
+                                sum += duration;
+                                count += 1;
+                                // println!("{} - {:?}", ip, duration);
+                            }
+                            Err(err) => {
+                                println!("{}({})", err, ips[el]);
+                                remove_list.push(el);
+                            }
                         }
                     }
+                    let mut offset = 0;
+                    for el in remove_list {
+                        ips.remove(el-offset);
+                        offset += 1;
+                    }
+                    if !sum.is_zero() {
+                        println!("Ping({}) - {:?}", i, sum/count);
+                    }
+                    std::thread::sleep(Duration::from_secs(1));
                 }
-                if !sum.is_zero() {
-                    println!("Avarage - {:?}", sum/count);
+                print!("Working hosts:");
+                for ip in ips {
+                    print!(" {}", ip);
                 }
+                println!();
                 scan_string_with_message("", false);
             }
             _ => {
